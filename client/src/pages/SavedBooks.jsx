9e2b12d105d76b450a1824e6fa6d71d12a3208c7
CJ <csh.joo77@gmail.com>
2024-07-07 21:45:18 -0400

    added changes to savedbooks.jsx so it to use row and col instead of column.

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
@@ -36,21 +36,21 @@ const SavedBooks = () => {
 
   return (
     <>
-      <Jumbotron fluid className="text-light bg-dark">
+      <Container fluid className="text-light bg-dark p-5">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
-      </Jumbotron>
+      </Container>
       <Container>
         <h2>
           {userData.savedBooks?.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
-        <CardColumns>
-          {userData.savedBooks?.map((book) => {
-            return (
-              <Card key={book.bookId} border="dark">
+        <Row>
+          {userData.savedBooks?.map((book) => (
+            <Col md="4" key={book.bookId}>
+              <Card border="dark">
                 {book.image ? (
                   <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant="top" />
                 ) : null}
@@ -63,9 +63,9 @@ const SavedBooks = () => {
                   </Button>
                 </Card.Body>
               </Card>
-            );
-          })}
-        </CardColumns>
+            </Col>
+          ))}
+        </Row>
       </Container>
     </>
   );
